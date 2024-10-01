5302c56edd9954bad76b24d5926704329b146860
AlexRapalini-IFTS24 <31650570@ifts24.edu.ar>
2024-09-30 21:16:28 -0300

    Modificacion codigo

--- listado.js
+++ listado.js
@@ -1,30 +1,30 @@
-let tasks = []; // crea un array vacio (listas)
+let clientes = []; // crea un array vacio (listas)
 let ID = 1;
 
-const form = document.querySelector(".form_task"); // Formulario
+const form = document.querySelector(".form_cliente"); // Formulario
 const nombreInput = document.querySelector("#nombre"); // Input
 const apellidoInput = document.querySelector("#apellido"); // Input2
 const taskList = document.querySelector("#taskList"); // Lista de li
 
 // Muestra las tareas en el HTML
-const renderTasks = () => {
-    taskList.innerHTML = ""; // Borrar toda la informacion del ul
+const renderClientes = () => {
+    clienteList.innerHTML = ""; // Borrar toda la informacion del ul
 
-    tasks.forEach((task) => {
+    clientes.forEach((cliente) => {
         // Dinamico con el texto ingresado en el input
         const html = ` 
       
-            <li data-id="${task.id}" class="tasks__item">     
-                <p id="entrada" class="${task.completa && "done"}"> ${task.id}</p>       
-                <p id="entrada" class="${task.completa && "done"}"> ${task.nombre}</p>
-                <p id="entrada" class="${task.completa && "done"}">${task.apellido}</p>  
+            <li data-id="${cliente.id}" class="clientes__item">     
+                <p id="entrada" class="${cliente.completa && "done"}"> ${cliente.id}</p>       
+                <p id="entrada" class="${cliente.completa && "done"}"> ${cliente.nombre}</p>
+                <p id="entrada" class="${cliente.completa && "done"}">${cliente.apellido}</p>  
                 <div>                                     
                     <i class="bx bx-check"></i>
                     <i class="bx bx-trash"></i>
                 </div/                                   
             </li>
     `;
-        taskList.innerHTML += html;
+        clienteList.innerHTML += html;
     })
 }
 
@@ -48,34 +48,34 @@ form.addEventListener("submit", async (event) => {
 
     if(!erroresValidacion){
        // console.log(txt_tarea);
-        const task = {
+        const cliente = {
             id: ID++, // nos da la cantidad de milisegundos desde 01/01/1970. Genero un numero unico
             nombre: nombre,
             apellido: apellido,
             completa: false,
         };
 
-       tasks.push(task); // Agrego la tarea a la lista de tareas
+       clientes.push(cliente); // Agrego la tarea a la lista de tareas
     
     
-        console.log(tasks);
+        console.log(clientes);
 
        // Almaceno las tareas en el localStorage
-        localStorage.setItem("tasks", JSON.stringify(tasks));
+        localStorage.setItem("clientes", JSON.stringify(clientes));
        // JSON.stringify(task) transforma un objeto JS  del tipo array
        // en un objeto JSON del tipo string
 
         // taskInput.value = ""; // limpiar el campo de la tarea (input)
         form.reset(); // limpiaria el formulario completo
 
-        renderTasks();
+        renderClientes();
     }
 })
 
 
 //Evento para el tachado
 
-taskList.addEventListener("click",(event) => {
+clienteList.addEventListener("click",(event) => {
     if (event.target.classList.contains("bx-check")){
         // console.log(event.target.closest("li"));
         //closest va a buscar el primer contenedor q coincida con li y lo va a traer.
@@ -85,15 +85,15 @@ taskList.addEventListener("click",(event) => {
         //Hacemos esto porque tenemos un array con elementos y deseo encontrar 
         //el elemento al q hice click
 
-        const task = tasks.find((task) => task.id == id);
+        const cliente = clientes.find((cliente) => cliente.id == id);
         //en cada tarea va a buscar si el id es igual al elemento q se hizo click
         // console.log(task);
 
         //Vamoa a cambiar el estado de la atarea
-        task.completa = !task.completa;
-        console.log(task);
+        cliente.completa = !cliente.completa;
+        console.log(cliente);
 
-        renderTasks();
+        renderClientes();
 
         //El problema de ejecutar un renderTasks() es q me carga la lista nuevamente
 
@@ -107,14 +107,14 @@ taskList.addEventListener("click",(event) => {
 
         //Pero si recargo la página las tareas pasan a estar incompletas
         //porque el localStorage no se actualizo
-        localStorage.setItem("tasks", JSON.stringify(tasks));
+        localStorage.setItem("clientes", JSON.stringify(clientes));
     };
 
     //BORRAR UNA TAREA
 
     if(event.target.classList.contains("bx-trash")){
         const id = event.target.closest("li").dataset.id;
-        const taskIndex = tasks.findIndex((task) => task.id == id);
+        const clienteIndex = clientes.findIndex((cliente) => cliente.id == id);
         
         //splice es una función q me permite manipular en JS los arrays
         //puedo añadir, eliminar o reemplazar los elementos de un array en una 
@@ -122,9 +122,9 @@ taskList.addEventListener("click",(event) => {
         //tasks.splice(taskIndex, 1);, le paso dos parámetros a la función
         //taskIndex es el q contiene el índice del elemento a manipular
         //1 indica la cantidad de elementos a eliminar en este caso
-        tasks.splice(taskIndex, 1);
+        clientes.splice(clienteIndex, 1);
         
-        localStorage.setItem("tasks", JSON.stringify(tasks));
+        localStorage.setItem("clientes", JSON.stringify(clientes));
         event.target.closest("li").remove();
     }
 });
@@ -136,9 +136,9 @@ taskList.addEventListener("click",(event) => {
 
 // Recupero lo almacenado en localStorage
 document.addEventListener("DOMContentLoaded", () => {
-    tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    clientes = JSON.parse(localStorage.getItem('clientes')) || [];
 
-    renderTasks();
+    renderClientes();
 });
 /*funcion para validar q solo ingrese letras*/
 function soloLetras(event){
